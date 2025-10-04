a1e3bc531ef08fb9e07d2b36f13c6e9dc2ffb7ba
HKJQ3 <2052346558@qq.com>
2025-10-04 16:45:15 +0800

    Update obfuscated_script-1759410916630.lua

--- obfuscated_script-1759410916630.lua
+++ obfuscated_script-1759410916630.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HKJQ3/-script/refs/heads/main/obfuscated_script-1759558717039.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HKJQ3/-script/refs/heads/main/obfuscated_script-1759567302529.lua"))()
